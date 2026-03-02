d10aaad99852cc28f3e183b84eac4cc72a8ccd33
ram <1243gkarthik@gmail.com>
2026-03-02 09:43:22 +0530

    added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -89,11 +89,11 @@ const AppRoutes = () => {
 // Main App
 function App() {
   return (
-    <HashRouter>
+    <Router>
       <AuthProvider>
         <AppRoutes />
       </AuthProvider>
-    </HashRouter>
+    </Router>
   );
 }
 
